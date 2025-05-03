a2c15bd9021cb5f29b3f901a4d8864764ea6c32a
Vincent Lacoste <vinzlac@gmail.com>
2025-05-03 12:14:39 +0200

    initial commit with only code skeleton

---
+++ src/main/java/com/vinzlac/kata/kafkapgspringbootjava/KafkapgspringbootjavaApplication.java
@@ -0,0 +1,13 @@
+package com.vinzlac.kata.kafkapgspringbootjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkapgspringbootjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkapgspringbootjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinzlac/kata/kafkapgspringbootjava/KafkapgspringbootjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinzlac.kata.kafkapgspringbootjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkapgspringbootjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
